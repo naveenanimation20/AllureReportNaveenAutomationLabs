f348813a26effd7767a4e6eddac2db4fd44a8305
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2019-02-28 07:42:40 +0530

    create deals page and bug fix in Home Page

---
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -0,0 +1,13 @@
+package com.qa.hubspot.pages;
+
+import com.qa.hubspot.Base.BasePage;
+
+public class DealsPage extends BasePage{
+	
+	public DealsPage(){
+		System.out.println("deals page");
+	}
+	
+	
+
+}
